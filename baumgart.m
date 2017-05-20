9a90ac46a6e790438d667687dcb2e4a81b1b444e
clapik1 <259809@pw.edu.pl>
2017-05-21 00:30:08 +0200

    single pendulum

--- baumgart.m
+++ baumgart.m
@@ -10,8 +10,8 @@ function [t, q, dq, ddq] = baumgart(q0, tK, s, m, I)
         ddq(iter, :) = H(t(iter), Y(iter,:)', s, m, I); 
     end
     
-    q = Y(:, 1:6)';
-    dq = Y(:, 7:12)';
+    q = Y(:, 1:3)';
+    dq = Y(:, 4:6)';
     ddq = ddq(:, size(q0,1)+1:end)';
 end
 
@@ -19,13 +19,13 @@ function dY= H(t, Y, s, m, I)
     alfa=5;
     beta=5;
     
-    q = Y(1:6, :);
-    dq = Y(7:12, :);
+    q = Y(1:3, :);
+    dq = Y(4:6, :);
     [F, Fq, G, M, Q] = macierze(s, m, I, q, dq);
-    A=[M, Fq'; Fq, zeros(4, 4)];
+    A=[M, Fq'; Fq, zeros(2, 2)];
     b=[Q; G - 2*alfa*Fq*dq - beta^2*F];
 
     x = A\b;
-    dY(1:6, 1) = dq;
-    dY(7:12) = x(1:6, 1);
+    dY(1:3, 1) = dq;
+    dY(4:6) = x(1:3, 1);
 end
--- pendulum.m
+++ pendulum.m
@@ -1,22 +1,23 @@
-m = [10; 5];
-s = [0.2; 0.1];
-I = [m(1) * s(1)^2 / 12; m(2) * s(2)^2 / 12];
+m = [10];
+s = [0.2];
+I = [m(1) * s(1)^2 / 12];
 
-q0 = [s(1)/2; 0; 0; s(1)+s(2)/2; 0; 0];
-tK = 20;
+q0 = [s(1)/2; 0; 0];
+tK = 10;
 
 [t, q, dq, ddq] = baumgart(q0, tK, s, m, I);
 
 figure('units','points','Position', [10, 10, 500, 700])
 anim = subplot(1, 2, 1);
 ener = subplot(1, 2, 2);
-set(anim, 'units','points','Position', [25, 25, 450, 450])
-set(ener, 'units','points','Position', [25, 525, 450, 150])
+set(anim, 'units','points','Position', [50, 25, 440, 450])
+set(ener, 'units','points','Position', [50, 525, 440, 150])
 axis(anim, [-0.5 0.5 -0.5 0.5])
 
 g = 9.81;
-e = g * (m(1)*q(2,:) + m(2)*q(5,:)) + (m(1)*(dq(1,:).^2 + dq(2,:).^2) + m(2)*(dq(4,:).^2 + dq(5,:).^2) + I(1)*dq(3,:).^2 + I(2)*dq(6,:).^2) / 2;
+e = g * m(1)*q(2,:) + (m(1)*(dq(1,:).^2 + dq(2,:).^2) + I(1)*dq(3,:).^2) / 2;
 plot(ener, t, e);
+plot(ener, t, g * m(1)*q(2,:));
 
 for i = 1:size(q, 2) - 1
     x1 = q(1, i) - s(1)*cos(q(3, i))/2;
@@ -24,15 +25,7 @@ for i = 1:size(q, 2) - 1
     x2 = q(1, i) + s(1)*cos(q(3, i))/2;
     y2 = q(2, i) + s(1)*sin(q(3, i))/2;
     
-    x3 = q(4, i) - s(2)*cos(q(6, i))/2;
-    y3 = q(5, i) - s(2)*sin(q(6, i))/2;
-    x4 = q(4, i) + s(2)*cos(q(6, i))/2;
-    y4 = q(5, i) + s(2)*sin(q(6, i))/2;
-    
     h1 = line(anim, [x1 x2], [y1 y2]);
-    h2 = line(anim, [x3 x4], [y3 y4]);
     pause(t(i + 1) - t(i))
-    disp(q(6,i))
     delete(h1)
-    delete(h2)
 end
