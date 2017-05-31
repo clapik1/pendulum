28090f5704deaff2747e2708bd12e7bf6ab7aa0b
clapik1 <259809@pw.edu.pl>
2017-06-01 01:17:52 +0200

    added plot for norms of F, dF, ddF

--- baumgart.m
+++ baumgart.m
@@ -1,23 +1,33 @@
-function [t, q, dq, ddq] = baumgart(q0, tK, s, m, I)
+function [t, q, dq, ddq, normF, normdF, normddF] = baumgart(q0, tK, s, m, I)
     t0 = 0;
     dq0 = zeros(size(q0, 1), 1);
     Y0 = [q0; dq0];
 
     [t, Y] = ode45(@(t, Y) H(t, Y, s, m, I), [t0 tK], Y0);
 
-    ddq=zeros(size(Y,1), size(Y,2));
-    for iter=1:size(Y,1)-1
-        ddq(iter, :) = H(t(iter), Y(iter,:)', s, m, I); 
+    temp = zeros(size(Y,1), size(Y,2));
+    for iter=1:size(Y,1)
+        temp(iter, :) = H(t(iter), Y(iter,:)', s, m, I); 
     end
     
     q = Y(:, 1:6)';
     dq = Y(:, 7:12)';
-    ddq = ddq(:, size(q0,1)+1:end)';
+    ddq = temp(:, 7:12)';
+    
+    normF = zeros(size(t));
+    normdF = zeros(size(t));
+    normddF = zeros(size(t));
+    for i = 1:size(t)
+        [F, Fq, G] = macierze(s, m, I, q(:, i), dq(:, i));
+        normF(i) = norm(F);
+        normdF(i) = norm(Fq * dq(:, i)); % tu brakuje fi czesciowo po t
+        normddF(i) = norm(Fq * ddq(:, i) - G);
+    end
 end
 
 function dY= H(t, Y, s, m, I)
-    alfa=5;
-    beta=5;
+    alfa = 5;
+    beta = 5;
     
     q = Y(1:6, :);
     dq = Y(7:12, :);
--- pendulum.m
+++ pendulum.m
@@ -3,21 +3,21 @@ s = [0.2; 0.1];
 I = [m(1) * s(1)^2 / 12; m(2) * s(2)^2 / 12];
 
 q0 = [s(1)/2; 0; 0; s(1)+s(2)/2; 0; 0];
-tK = 20;
+tK = 2;
 
-[t, q, dq, ddq] = baumgart(q0, tK, s, m, I);
-
-figure('units','points','Position', [10, 10, 500, 700])
-anim = subplot(1, 2, 1);
-ener = subplot(1, 2, 2);
-set(anim, 'units','points','Position', [25, 25, 450, 450])
-set(ener, 'units','points','Position', [25, 525, 450, 150])
-axis(anim, [-0.5 0.5 -0.5 0.5])
+[t, q, dq, ddq, normF, normdF, normddF] = baumgart(q0, tK, s, m, I);
 
 g = 9.81;
 e = g * (m(1)*q(2,:) + m(2)*q(5,:)) + (m(1)*(dq(1,:).^2 + dq(2,:).^2) + m(2)*(dq(4,:).^2 + dq(5,:).^2) + I(1)*dq(3,:).^2 + I(2)*dq(6,:).^2) / 2;
-plot(ener, t, e);
 
+figure
+ener = subplot(2, 1, 1);
+plot(ener, t, e, t, g * (m(1)*q(2,:) + m(2)*q(5,:)))
+normy = subplot(2, 1, 2);
+plot(normy, t, normF, t, normdF, t, normddF)
+
+figure
+axis([-0.5 0.5 -0.5 0.5])
 for i = 1:size(q, 2) - 1
     x1 = q(1, i) - s(1)*cos(q(3, i))/2;
     y1 = q(2, i) - s(1)*sin(q(3, i))/2;
@@ -29,10 +29,9 @@ for i = 1:size(q, 2) - 1
     x4 = q(4, i) + s(2)*cos(q(6, i))/2;
     y4 = q(5, i) + s(2)*sin(q(6, i))/2;
     
-    h1 = line(anim, [x1 x2], [y1 y2]);
-    h2 = line(anim, [x3 x4], [y3 y4]);
+    h1 = line([x1 x2], [y1 y2]);
+    h2 = line([x3 x4], [y3 y4]);
     pause(t(i + 1) - t(i))
-    disp(q(6,i))
     delete(h1)
     delete(h2)
 end
